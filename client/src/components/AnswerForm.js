d9ce1636072ff4ef9d6542c11cff0882daf69c62
Gavin Dreyer <gavindreyer1@gmail.com>
2019-09-25 08:50:32 -0700

    Added chart.js and a lifetime answered state

--- client/src/components/AnswerForm.js
+++ client/src/components/AnswerForm.js
@@ -34,29 +34,49 @@ const incorrect = new UIfx (
 const dataArray = [
     {'Conjugate Caminar to First Person Present': 'Camino'},
     {'Conjugate Hablar to Third Person Present': 'Habla'},
-    {'Conjugate Comer to Second Person Present': 'Comes'}
+    {'Conjugate Comer to Second Person Present': 'Comes'},
+    {'Conjugate Beber to Third Person Present Plural': 'Beben'},
+    {'Conjugate Mirar to First Person Present': 'Miro'},
+    {'Conjugate Pagar to Third Person Present Plural': 'Pagan'},
+    {'Conjugate Parar to Second Person Present': 'Paras'},
+    {'Conjugate Ganar to First Person Present Plural': 'Ganamos'},
+    {'Conjugate Estar to Third Person Present Plural': 'Estan'},
+    {'Conjugate Tener to First Person Present': 'Tengo'},
+    {'Conjugate Ir to Second Person Present': 'Vas'}
 ]
 
 const AnswerForm = props => {
+    
     const [streak, setStreak] = useState(0)
-    const [currentQ, setCurrentQ] = useState(Object.keys(dataArray[streak]).join())
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [totalQs, setTotalQs] = useState(0)
+    const [currentQ, setCurrentQ] = useState(Object.keys(dataArray[currentIndex]).join())
     const [answers, setAnswers] = useState({
         answerBar: ''
       })
 
     const addOne = e => {
         setStreak(streak + 1)
-        setCurrentQ(Object.keys(dataArray[streak + 1]).join())
+        setCurrentIndex(currentIndex + 1)
+        setTotalQs(totalQs + 1)
+        setCurrentQ(Object.keys(dataArray[currentIndex + 1]).join())
+        if(currentIndex === 9){
+            setCurrentQ(Object.keys(dataArray[0]).join())
+            setCurrentIndex(0)
+        }
     }
 
     function checkAnswer(){
-       if(answers.answerBar === Object.values(dataArray[streak]).join()){
+        if(answers.answerBar === Object.values(dataArray[currentIndex]).join()){
             addOne()
             correct.play()
-       } else {
-         incorrect.play()
+        }else {
+            setStreak(0)
+            incorrect.play()
        }
     }
+
+    console.log(currentIndex)
 
     const handleChange = event => {
         setAnswers({[event.target.name]: event.target.value});
@@ -72,7 +92,7 @@ const AnswerForm = props => {
     
     return (
         <div>
-            {streak}  
+            {streak} {totalQs}
             <form onSubmit={event => handleSubmit(event)}>
                 <label htmlFor='answerBar'>
                     Answer:
@@ -85,7 +105,7 @@ const AnswerForm = props => {
                 <button onSubmit={() => handleSubmit()} onClick={checkAnswer}>Submit!</button>
             </form>
             <p>{currentQ}</p>
-            <Charts />
+            <Charts streak={streak} currentIndex={currentIndex}/>
       </div>
     )
 }
--- client/src/components/Charts.js
+++ client/src/components/Charts.js
@@ -1,45 +1,81 @@
 import React, { useState } from 'react'
 import { Line } from 'react-chartjs-2'
 import styled from 'styled-components'
+import Chart from 'chart.js'
 
 import '../../src/App.css'
 
 const DivCon = styled.div`
     background: white;
-    border: 2px solid red;
 `;
 
+const data = {
+    labels: [1, 2, 3],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
 
 
-function Charts () {
-    const [dataSet, setDataSet] = useState({
-        data: {
-            labels: ['1', '2', '3', '4', '5'],
-            datasets: [
-                {
-                    label: 'Videos Made',
-                    backgroundColor: 'rgba(255, 0, 255, 0.75)',
-                    data: [4, 5, 1, 10, 32, 2, 12]
-                },
-                {
-                    label: 'Subscriptions',
-                    backgroundColor: 'rgba(0, 255, 0, 0.75)',
-                    data: [14, 15, 21, 0, 12, 4, 2]
-                }
-            ]
-        }
-    })
+function Charts (props) {
+    const [dataSet, setDataSet] = useState()
+    // console.log(props.streak)
 
-    console.log(dataSet)
+    const data = {
+        labels: [1, 2, 3, 4],
+        datasets: [
+          {
+            label: 'My First dataset',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [props.streak, props.currentIndex, props.streak, props.currentIndex]
+          }
+        ]
+      };
 
     return (
         <DivCon style={{ position: 'relative', width: 600, height: 350 }}>
-            <h3>Chart Samples</h3>
+            <h3>Consistency</h3>
             <Line
-                options={{
-                    responsive: true
-                }}
-                data={dataSet}
+                // options={{
+                //     responsive: true
+                // }}
+                data={data}
             />
 
         </DivCon>
@@ -47,3 +83,40 @@ function Charts () {
 }
 
 export default Charts
+
+// import React from 'react';
+// import {Pie} from 'react-chartjs-2';
+
+// const data = {
+// 	labels: [
+// 		'Red',
+// 		'Blue',
+// 		'Yellow'
+// 	],
+// 	datasets: [{
+// 		data: [300, 50, 100],
+// 		backgroundColor: [
+// 		'#FF6384',
+// 		'#36A2EB',
+// 		'#FFCE56'
+// 		],
+// 		hoverBackgroundColor: [
+// 		'#FF6384',
+// 		'#36A2EB',
+// 		'#FFCE56'
+// 		]
+// 	}]
+// };
+
+// export default React.createClass({
+//   displayName: 'PieExample',
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>Pie Example</h2>
+//         <Pie data={data} />
+//       </div>
+//     );
+//   }
+// });
