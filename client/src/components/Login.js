dedc3bbc7000afc8e7317322e4e69cedcb29a1ec
Ronny Alvarado <rsalvarado777@gmail.com>
2019-09-27 11:54:58 -0500

    Made modifications to login/signup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,87 +1,109 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import { withFormik, Form, Field } from "formik"
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { withFormik, Form, Field } from "formik";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import * as Yup from "yup";
 
-import '../App.css'
+import "../App.css";
 
 const DivCon = styled.div`
-    display: flex;
-    justify-content: center;
-    align-content: center; 
+  display: flex;
+  justify-content: center;
+  align-content: center;
 `;
 
 const DivCon2 = styled.div`
-    box-shadow: 1px 1px 5px 5px;
-    width: 60%;
-    height: 25rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    background-color: white;
-    
-    
-    
+  box-shadow: 1px 1px 5px 5px;
+  width: 60%;
+  height: 30rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: white;
 `;
 
 const DivCon3 = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: 0 0 0 20rem;
-    height: 30vh;
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 0 20rem;
+  height: 30vh;
 `;
 
-const Login = ({ status }) => {
-    const [users, setUsers] = useState([])
+const Login = ({ status, handleSubmit, history, errors, touched }) => {
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        if(status) {
-            setUsers([...users, status])
-        }
-    }, [status])
+  useEffect(() => {
+    if (status) {
+      setUsers([...users, status]);
+      history.push("/conjugator");
+    }
+  }, [status]);
 
-    return (
-        <DivCon>
-            <DivCon2 className='mainCon'>
-                <DivCon3>
-                    <Form className="FormCon">
-                        <div className="fields">
-                            <Field className="FieldCon" type="text" name="login" placeholder="Username" />
-                        </div>
-                        <div className="fields">
-                            <Field className="FieldCon" type="password" name="password" placeholder="Password" />
-                        </div>  
-                        <Link to='/conjugator' className="LinkStyle">Login</Link>
-                    </Form>
-                    
-                    <Link className="LinkStyle" to="/sign-up">Sign Up!</Link>
-                </DivCon3>
-            </DivCon2>
-        </DivCon>
-        
-    )
-}
+  return (
+    <DivCon>
+      <DivCon2 className="mainCon">
+        <DivCon3>
+          <Form className="FormCon">
+            <div className="fields">
+              <Field
+                className="FieldCon"
+                type="text"
+                name="email"
+                placeholder="Email"
+              />
+              {touched.email && errors.email && (
+                <p className="error">{errors.email}</p>
+              )}
+            </div>
+            <div className="fields">
+              <Field
+                className="FieldCon"
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+              {touched.password && errors.password && (
+                <p className="error">{errors.password}</p>
+              )}
+            </div>
+            <button onClick={handleSubmit} className="LinkStyle">
+              Login
+            </button>
+          </Form>
+
+          <Link className="LinkStyle" to="/sign-up">
+            Sign Up!
+          </Link>
+        </DivCon3>
+      </DivCon2>
+    </DivCon>
+  );
+};
 
 const FormikLogin = withFormik({
-    mapPropsToValues({ login, password }) {
-        return {
-            login: login || '',
-            password: password || ''
-        }
-    },
-    handleSubmit(values, {setStatus, resetForm }) {
-        axios
-            .post(`https://conju.herokuapp.com/api/auth/login`, values)
-            .then(response => {
-                setStatus(response.data)
-                console.log(response.data)
-            })
-            .catch(error => {
-                console.elog(error)
-            })
-            resetForm()
-    }
-})(Login)
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string().required("You must put an email"),
+    password: Yup.string().required("You must put a password")
+  }),
+  handleSubmit(values, { setStatus, resetForm }) {
+    axios
+      .post(`https://conju.herokuapp.com/api/auth/login`, values)
+      .then(response => {
+        setStatus(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    resetForm();
+  }
+})(Login);
 
-export default FormikLogin
+export default FormikLogin;
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,82 +1,102 @@
-import React from 'react';
+import React from "react";
 import { Form, Field, withFormik } from "formik";
 import styled from "styled-components";
-import axios from 'axios'
+import axios from "axios";
 import { Link } from "react-router-dom";
 import Gator from "../assets/gator_2.jpg";
 
-
 const SignUpDiv = styled.div`
-   display: flex;
-   justify-content: center;
-   align-content: center;
-`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+`;
 const SignUpWrapper1 = styled.div`
-    box-shadow: 1px 1px 5px 5px;
-    width: 60%;
-    height: 25rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    background-color: white;
-`
+  box-shadow: 1px 1px 5px 5px;
+  width: 60%;
+  height: 25rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: white;
+`;
 const SignUpWrapper2 = styled.div`
-    display: flex;
-    justify-content: space-around;
-    margin: 1rem;
-    margin: 10 0 0 0rem;
-    margin-top: 4rem;
-    height: 100%;
-`
+  display: flex;
+  justify-content: space-around;
+  margin: 1rem;
+  margin: 10 0 0 0rem;
+  margin-top: 4rem;
+  height: 100%;
+`;
 
 const GatorPic = styled.img`
-    width: 50%;
-    height: 90%;
-    max-width: 300px;
-`
+  width: 50%;
+  height: 90%;
+  max-width: 300px;
+`;
 
-const SignUp = () => {
-   return (
-       <SignUpDiv>
-           <SignUpWrapper1>
-              <SignUpWrapper2>
-                <Form className="FormCon2">
-                    <div className="fields">
-                        <Field className="FieldCon" type="text" name="user" placeholder="Username" />
-                    </div>
-                    <div className="fields">
-                        <Field className="FieldCon" type="email" name="email" placeholder="Email" />
-                    </div>
-                    <div className="fields">
-                        <Field className="FieldCon" type="password" name="password" placeholder="Password" />
-                    </div>
-                    <Link to='/conjugator' className="LinkStyle">Submit</Link>
-                </Form>
-                <GatorPic src={Gator} alt="happy gator" />
-              </SignUpWrapper2>
-           </SignUpWrapper1>
-       </SignUpDiv>
-   )
+const SignUp = props => {
+  return (
+    <SignUpDiv>
+      <SignUpWrapper1>
+        <SignUpWrapper2>
+          <Form className="FormCon2">
+            <div className="fields">
+              <Field
+                className="FieldCon"
+                type="text"
+                name="name"
+                placeholder="Username"
+              />
+            </div>
+            <div className="fields">
+              <Field
+                className="FieldCon"
+                type="email"
+                name="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="fields">
+              <Field
+                className="FieldCon"
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+            </div>
+            <button onClick={props.handleSubmit} className="LinkStyle">
+              Submit
+            </button>
+            <Link to="/" className="LinkStyle">
+              Login
+            </Link>
+          </Form>
+          <GatorPic src={Gator} alt="happy gator" />
+        </SignUpWrapper2>
+      </SignUpWrapper1>
+    </SignUpDiv>
+  );
 };
 const FormikSignUp = withFormik({
-   mapPropsToValues({user, email, password}) {
-       return {
-           user: user || "",
-           email: email || "",
-           password: password || ""
-   }
- }, handleSubmit(values, {setStatus, resetForm }) {
+  mapPropsToValues({ name, email, password }) {
+    return {
+      name: name || "",
+      email: email || "",
+      password: password || ""
+    };
+  },
+  handleSubmit(values, { setStatus, resetForm }) {
     axios
-        .post(`https://conju.herokuapp.com/api/auth/register`, values)
-        .then(response => {
-            setStatus(response.data)
-            console.log(response.data)
-        })
-        .catch(error => {
-            console.elog(error)
-        })
-        resetForm()
-}
+      .post(`https://conju.herokuapp.com/api/auth/register`, values)
+      .then(response => {
+        setStatus(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    resetForm();
+  }
 })(SignUp);
-export default FormikSignUp
+export default FormikSignUp;
